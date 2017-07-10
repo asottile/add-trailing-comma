61a4d747036b8c522a1a6648baafad310af8b65d
Anthony Sottile <asottile@umich.edu>
2017-07-09 20:50:53 -0700

    Add support for adding trailing commas to literals

--- tests/add_trailing_comma_test.py
+++ tests/add_trailing_comma_test.py
@@ -7,8 +7,11 @@ import sys
 
 import pytest
 
-from add_trailing_comma import _fix_calls
+from add_trailing_comma import _fix_commas
 from add_trailing_comma import main
+
+
+xfailif_lt_py35 = pytest.mark.xfail(sys.version_info < (3, 5), reason='py35+')
 
 
 @pytest.mark.parametrize(
@@ -42,7 +45,7 @@ from add_trailing_comma import main
     ),
 )
 def test_fix_calls_noops(src):
-    ret = _fix_calls(src, py35_plus=False)
+    ret = _fix_commas(src, py35_plus=False)
     assert ret == src
 
 
@@ -59,7 +62,7 @@ def test_ignores_invalid_ast_node():
         '    """\n'
         ')'
     )
-    assert _fix_calls(src, py35_plus=False) == src
+    assert _fix_commas(src, py35_plus=False) == src
 
 
 def test_py35_plus_rewrite():
@@ -68,7 +71,7 @@ def test_py35_plus_rewrite():
         '    *args\n'
         ')'
     )
-    ret = _fix_calls(src, py35_plus=True)
+    ret = _fix_commas(src, py35_plus=True)
     assert ret == (
         'x(\n'
         '    *args,\n'
@@ -76,10 +79,14 @@ def test_py35_plus_rewrite():
     )
 
 
-@pytest.mark.xfail(sys.version_info < (3, 5), reason='py35+ only feature')
+@xfailif_lt_py35
 @pytest.mark.parametrize(
     'syntax',
     (
+        '(1, 2, *a)\n',
+        '[1, 2, *a]\n',
+        '{1, 2, *a}\n',
+        '{1: 2, **k}\n',
         'y(*args1, *args2)\n',
         'y(**kwargs1, **kwargs2)\n',
     ),
@@ -87,7 +94,159 @@ def test_py35_plus_rewrite():
 def test_auto_detected_py35_plus_rewrite(syntax):
     src = syntax + 'x(\n    *args\n)'
     expected = syntax + 'x(\n    *args,\n)'
-    assert _fix_calls(src, py35_plus=False) == expected
+    assert _fix_commas(src, py35_plus=False) == expected
+
+
+@pytest.mark.parametrize(
+    ('src', 'expected'),
+    (
+        (
+            'x(\n'
+            '    1\n'
+            ')',
+
+            'x(\n'
+            '    1,\n'
+            ')',
+        ),
+        (
+            'x(\n'
+            '    kwarg=5\n'
+            ')',
+
+            'x(\n'
+            '    kwarg=5,\n'
+            ')',
+        ),
+        (
+            'foo()(\n'
+            '    1\n'
+            ')',
+
+            'foo()(\n'
+            '    1,\n'
+            ')',
+        ),
+    ),
+)
+def test_fixes_calls(src, expected):
+    assert _fix_commas(src, py35_plus=False) == expected
+
+
+@pytest.mark.parametrize(
+    'src',
+    (
+        '(1, 2, 3, 4)',
+        '[1, 2, 3, 4]',
+        '{1, 2, 3, 4}',
+        '{1: 2, 3: 4}',
+    ),
+)
+def test_noop_one_line_literals(src):
+    assert _fix_commas(src, py35_plus=False) == src
+
+
+@pytest.mark.parametrize(
+    ('src', 'expected'),
+    (
+        (
+            'x = [\n'
+            '    1\n'
+            ']',
+
+            'x = [\n'
+            '    1,\n'
+            ']',
+        ),
+        (
+            'x = {\n'
+            '    1\n'
+            '}',
+
+            'x = {\n'
+            '    1,\n'
+            '}',
+        ),
+        (
+            'x = {\n'
+            '    1: 2\n'
+            '}',
+
+            'x = {\n'
+            '    1: 2,\n'
+            '}',
+        ),
+        (
+            'x = (\n'
+            '    1,\n'
+            '    2\n'
+            ')',
+
+            'x = (\n'
+            '    1,\n'
+            '    2,\n'
+            ')',
+        ),
+    ),
+)
+def test_fixes_literals(src, expected):
+    assert _fix_commas(src, py35_plus=False) == expected
+
+
+@xfailif_lt_py35
+@pytest.mark.parametrize(
+    ('src', 'expected'),
+    (
+        (
+            'x = {\n'
+            '    1, *y\n'
+            '}',
+
+            'x = {\n'
+            '    1, *y,\n'
+            '}',
+        ),
+        (
+            'x = [\n'
+            '    1, *y\n'
+            ']',
+
+            'x = [\n'
+            '    1, *y,\n'
+            ']',
+        ),
+        (
+            'x = (\n'
+            '    1, *y\n'
+            ')',
+
+            'x = (\n'
+            '    1, *y,\n'
+            ')',
+        ),
+        (
+            'x = {\n'
+            '    1: 2, **y\n'
+            '}',
+
+            'x = {\n'
+            '    1: 2, **y,\n'
+            '}',
+        ),
+    ),
+)
+def test_fixes_py35_plus_literals(src, expected):
+    assert _fix_commas(src, py35_plus=False) == expected
+
+
+def test_noop_tuple_literal_without_braces():
+    src = (
+        'x = \\\n'
+        '    1, \\\n'
+        '    2, \\\n'
+        '    3'
+    )
+    assert _fix_commas(src, py35_plus=False) == src
 
 
 def test_main_trivial():
