47aa870cde65d699237d345df17bfb1ca03bd3f7
Anthony Sottile <asottile@umich.edu>
2017-07-16 15:17:32 -0700

    v0.5.0

--- README.md
+++ README.md
@@ -20,7 +20,7 @@ Sample `.pre-commit-config.yaml`:
 
 ```yaml
 -   repo: https://github.com/asottile/add-trailing-comma
-    sha: v0.4.3
+    sha: v0.5.0
     hooks:
     -   id: add-trailing-comma
 ```
--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='add_trailing_comma',
     description='Automatically add trailing commas to calls and literals',
     url='https://github.com/asottile/add_trailing_comma',
-    version='0.4.3',
+    version='0.5.0',
     author='Anthony Sottile',
     author_email='asottile@umich.edu',
     classifiers=[
