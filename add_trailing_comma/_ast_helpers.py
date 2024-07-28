a07ae32a72d074b91767e806ae53e1ffe8d7c933
Anthony Sottile <asottile@umich.edu>
2024-07-28 14:18:17 -0400

    use a Protocol for ast_to_offset

--- add_trailing_comma/_ast_helpers.py
+++ add_trailing_comma/_ast_helpers.py
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 import ast
 import warnings
+from typing import Protocol
 
 from tokenize_rt import Offset
 
@@ -12,5 +13,12 @@ def ast_parse(contents_text: str) -> ast.Module:
         return ast.parse(contents_text.encode())
 
 
-def ast_to_offset(node: ast.AST) -> Offset:
+class _HasOffsetInfo(Protocol):
+    @property
+    def lineno(self) -> int: ...
+    @property
+    def col_offset(self) -> int: ...
+
+
+def ast_to_offset(node: _HasOffsetInfo) -> Offset:
     return Offset(node.lineno, node.col_offset)
--- add_trailing_comma/_plugins/calls.py
+++ add_trailing_comma/_plugins/calls.py
@@ -34,7 +34,7 @@ def visit_Call(
         state: State,
         node: ast.Call,
 ) -> Iterable[tuple[Offset, TokenFunc]]:
-    argnodes = [*node.args, *node.keywords]
+    argnodes: list[ast.expr | ast.keyword] = [*node.args, *node.keywords]
     arg_offsets = set()
     for argnode in argnodes:
         offset = ast_to_offset(argnode)
